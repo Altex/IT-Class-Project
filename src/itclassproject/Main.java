746ca28f1a073a187b271de45eb708b7c1d5cf42
Daniel Levin <daniellevin2607@gmail.com>
2012-03-27 02:45:50 -0400

    Added a trivial line

--- src/itclassproject/Main.java
+++ src/itclassproject/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package itclassproject;
 
 /**
@@ -15,7 +14,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Trivial change");
     }
-
 }
